448fdc69749af56d547003dc4aac81cf4825f361
Timzhouyes <1175566718@qq.com>
2020-01-06 09:29:17 +0800

    Add generic

--- ZHMLearnJava/src/GenericStudy/GenericClass/GenericClassTesting.java
+++ ZHMLearnJava/src/GenericStudy/GenericClass/GenericClassTesting.java
@@ -2,7 +2,7 @@ package GenericStudy.GenericClass;
 
 
 public class GenericClassTesting {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         GenericClassExample<Integer> genericInteger = new GenericClassExample<Integer>(123);
         GenericClassExample<String> genericString = new GenericClassExample<String>("Hello");
 
@@ -15,6 +15,25 @@ public class GenericClassTesting {
 
         System.out.println("Generic is " + generic.getKey());
         System.out.println("Generic2 is " + generic2.getKey());
+
+        System.out.println("*********Below is Wildcard example***************");
+        GenericClassExample<Integer> gInteger = new GenericClassExample<>(123);
+        GenericClassExample<Number> gNumber = new GenericClassExample<>(456);
+        showValue(gInteger);
+
+        System.out.println("************");
+//        GenericClassExample gExample = new GenericClassExample();
+        Object obj = genericMethod(Class.forName("GenericStudy.GenericClass.GenericClassImpl"));
+        System.out.println(obj);
+    }
+
+    public static void showValue(GenericClassExample<?> obj) {
+        System.out.println("Key value is " + obj.getKey());
+    }
+
+    public static <SB> SB genericMethod(Class<SB> sbClass) throws InstantiationException, IllegalAccessException {
+        SB instance = sbClass.newInstance();
+        return instance;
     }
 
 }
